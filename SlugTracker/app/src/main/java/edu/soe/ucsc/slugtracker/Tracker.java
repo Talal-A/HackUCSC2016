851df0efd046b882d36facebd7dbca97178b35ba
talal.abouhaiba <talal.abouhaiba@gmail.com>
2016-01-29 17:15:28 -0800

    Partial Issue 4

--- SlugTracker/app/src/main/java/edu/soe/ucsc/slugtracker/Tracker.java
+++ SlugTracker/app/src/main/java/edu/soe/ucsc/slugtracker/Tracker.java
@@ -1,13 +1,44 @@
 package edu.soe.ucsc.slugtracker;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Tracker extends AppCompatActivity {
+/**
+ * Created by talal.abouhaiba on 1/29/16.
+ *
+ * This class is designed to allow the user to "add" meal items to their running calorie total.
+ */
+
+public class Tracker extends AppCompatActivity implements View.OnClickListener {
+
+    SharedPreferences savedInfo;
+    private SharedPreferences.Editor settingsEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
+
+        savedInfo = getSharedPreferences("savedInfo", 0);
+
+        settingsEditor = savedInfo.edit();
+
+        Button add1 = (Button) findViewById(R.id.add1);
+        add1.setOnClickListener(this);
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.add1:
+                System.out.println("Test");
+                break;
+        }
+    }
+
+
 }
