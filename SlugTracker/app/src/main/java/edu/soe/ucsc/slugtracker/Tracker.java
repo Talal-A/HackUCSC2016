3f7a77d83bfe40839a6639ded34bf5da76f8adf8
talal.abouhaiba <talal.abouhaiba@gmail.com>
2016-01-30 17:49:32 -0800

    Merge Resolved

--- SlugTracker/app/src/main/java/edu/soe/ucsc/slugtracker/Tracker.java
+++ SlugTracker/app/src/main/java/edu/soe/ucsc/slugtracker/Tracker.java
@@ -1,14 +1,12 @@
 package edu.soe.ucsc.slugtracker;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,7 +18,6 @@ import android.widget.TextView;
 import org.jsoup.*;
 
 import org.jsoup.nodes.Element;
-import org.w3c.dom.Document;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -29,9 +26,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Calendar;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by talal.abouhaiba on 1/29/16.
@@ -42,27 +36,28 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Tracker extends ListActivity implements View.OnClickListener {
 
     SharedPreferences savedInfo;
+
+    private int calCount;
+    private int locationNum;
+
     private SharedPreferences.Editor settingsEditor;
     TextView count;
-    private int calCount;
     private List<FoodObject> foodItems;
     ArrayAdapter arrayAdapter;
     ProgressDialog pd;
     private Handler handler;
-    private FoodDataBase foodData;
+
     private FoodObject[] stack = new FoodObject[500];
     int size = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
 
-        foodData = new FoodDataBase(this);
-        foodData.insertFood("apple", 50, 50.5f, 50.5f, 50.5f);
-        foodData.getNutrition(1);
-
+        Button changeLocation = (Button) findViewById(R.id.changeLocation);
+        changeLocation.setOnClickListener(this);
 
         savedInfo = getSharedPreferences("savedInfo", 0);
 
@@ -72,65 +67,127 @@ public class Tracker extends ListActivity implements View.OnClickListener {
         count = (TextView) findViewById(R.id.textView);
 
         calCount = savedInfo.getInt("Calories", 0);
+        locationNum = savedInfo.getInt("LocationNum", 5);
         updateCount();
 
-        // Alocate memory for list
+        // Allocate memory for list
 
         foodItems = new ArrayList<>();
-        System.out.println("begin");
-        //new Task().execute();
 
-        pd = ProgressDialog.show(this,"", "Loading", true, false);
+        ListView lv = getListView();
+        lv.setAdapter(arrayAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                calCount += foodItems.get(position).getCal();
+                push(foodItems.get(position));
+                updateCount();
+            }
+        });
+
+        updateList();
+
+    } // end onCreate
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.changeLocation:
+                presentLocations();
+                break;
+
+        } // end switch
+
+    } // end onClick
+
+    private void presentLocations() {
+
+        final List<String> locations = new ArrayList<>();
+
+        locations.add("Porter/Kresge");
+        locations.add("Cowell/Stevenson");
+        locations.add("Crown/Merrill");
+        locations.add("Eight/Oakes");
+        locations.add("Nine/Ten");
+
+        AlertDialog.Builder locationList = new AlertDialog.Builder(this);
+        locationList.setTitle("Select a Location");
+
+        locationList.setItems(locations.toArray(new CharSequence[locations.size()]), new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                setLocation(webLocation(locations.get(which)));
+            }
+        });
+
+        locationList.show();
+
+    } // end presentLocations
+
+    public int getCurrentLocation() {
+        return locationNum;
+    }
+
+
+    private void setLocation(int locationNumber) {
+
+        locationNum = locationNumber;
+        settingsEditor.putInt("LocationNum", locationNum);
+        settingsEditor.apply();
+
+        System.out.println("Setting location to: " + locationNum);
+        updateList();
+
+    }
+
+    private void updateList() {
+
+        pd = ProgressDialog.show(this, "", "Loading", true, false);
         handler = new Handler();
+
+        foodItems = null;
+        foodItems = new ArrayList<>();
 
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                System.out.println("in run");
                 Looper.prepare();
-
-                String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-                System.out.println(date);
-                System.out.println(mealTime());
-                String locationNumber = "05";       // User chosen
-                String currentMonth = date.substring(5, 7);          // From current date
-                String currentDay =  "29";//date.substring(8);           // From current date
-                String currentYear = date.substring(0, 4);        // From current date
-                String currentMeal = mealTime();   // User chosen
 
                 org.jsoup.nodes.Document doc = null;
 
                 // checks what day of week it is to determine whether college is closed or not
                 Calendar c = Calendar.getInstance();
                 int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-                int college = Integer.parseInt(locationNumber);
-                if((dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) &&
+                int college = getCurrentLocation();
+                if ((dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) &&
                         (college == 20 || college == 30)) {
                     System.out.println("This college is closed");
-                } else if((dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) &&
-                        currentMeal.equals("Breakfast")){
+                } else if ((dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) &&
+                        getMealTime().equals("Breakfast")) {
                     System.out.println("Breakfast is not served this day");
                 }
 
                 // FIXME: Should be in an else block, add else after we handle an "empty day"
-                    // Scrape info off site.
-                    try {
-                        doc = Jsoup.connect("http://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=" +
-                                locationNumber + "&dtdate=" + currentMonth + "%2F" + currentDay + "%2F" +
-                                currentYear + "&mealName=" + currentMeal).get();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
+                // Scrape info off site.
+                System.out.println("http://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=" +
+                        getCurrentLocation() + "&dtdate=" + getDateString() + "&mealName=" + getMealTime());
+                try {
+                    doc = Jsoup.connect("http://nutrition.sa.ucsc.edu/pickMenu.asp?locationNum=" +
+                            getCurrentLocation() + "&dtdate=" + getDateString() + "&mealName=" + getMealTime()).get();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
 
 
                 // Search for line with food names.
                 // String temp = doc.select("a[href]").id();
 
-                for(Element e: doc.select("a[href]"))
+                for (Element e : doc.select("a[href]"))
                     e.wrap("<foods></foods>");
 
                 // Searching through tags for just the names.
-                for(Element e: doc.getElementsByTag("foods")) {
+                for (Element e : doc.getElementsByTag("foods")) {
 
                     String current = e.select("a[href]").text();
                     String foodName = e.select("a[href]").text();
@@ -140,7 +197,7 @@ public class Tracker extends ListActivity implements View.OnClickListener {
 
                     String nutritionSite = e.select("a[href]").attr("abs:href");
 
-                    System.out.println(nutritionSite);
+
                     // parse needed
                     org.jsoup.nodes.Document doc2 = null;
 
@@ -152,28 +209,32 @@ public class Tracker extends ListActivity implements View.OnClickListener {
 
                     // Wraps the current food item with a nutrition tag so that we can parse them.
                     // Without this for loop we end up getting all of the food tags on a single line
-                    for(Element el: doc2.select("font"))
+                    for (Element el : doc2.select("font"))
                         el.wrap("<nutrition></nutrition>");
 
                     // Grabs the food name that is surrounded by the nutrition tag
 
                     // Allergen Information
-                    String[] tempAllergens;
-                    Boolean allergenBoolean = false;
-                    for(Element el: doc2.select("span")){
-                        String cur = el.select("span").text();
-                        System.out.println(cur);
-                        if (allergenBoolean){
-                            tempAllergens = cur.split(",");
-                            for(int i = 0; i < tempAllergens.length; i++){
-                                System.out.println("Allergen info: " + tempAllergens[i]);
-                            }
-                            break;
-                        }
-                        if(cur.contains("Allergens:" + "\u00a0")){
-                            allergenBoolean = true;
-                        }
+                    boolean milk = false, soy = false, treeOrPNut = false, gluten = false;
+                    boolean fishShell = false, egg = false, wheat = false;
 
+                    for (Element el : doc2.select("span")) {
+                        String cur = el.select("span").text();
+                        if (cur.contains("Egg"))
+                            egg = true;
+                        if (cur.contains("Soybean"))
+                            soy = true;
+                        if (cur.contains("Gluten"))
+                            gluten = true;
+                        if (cur.contains("Wheat"))
+                            wheat = true;
+                        if (cur.contains("Milk"))
+                            milk = true;
+                        if (cur.contains("Tree Nut") ||
+                                cur.contains("Peanut"))
+                            treeOrPNut = true;
+                        if (cur.contains("Fish") || cur.contains("fish"))
+                            fishShell = true;
                     }
 
                     boolean isProtein = false;
@@ -191,18 +252,20 @@ public class Tracker extends ListActivity implements View.OnClickListener {
                     boolean obtainedCalories = false;
 
 
-                    for(Element el: doc2.getElementsByTag("nutrition")) {
+                    for (Element el : doc2.getElementsByTag("nutrition")) {
 
 
                         String cur = el.select("font").text();
                         //System.out.println("TEST: " + cur);
 
+                        // Pushes FoodObject instances into a list of FoodObjects "foodItems"
                         if (obtainedCalories && obtainedCarbs && obtainedFat && obtainedProtein) {
-
-                            foodItems.add(new FoodObject(foodName,currentCal,currentProtein, currentFat, currentCarb));
-
+                            FoodObject tempFoodO = new FoodObject(foodName, currentCal,
+                                    currentProtein, currentFat, currentCarb);
+                            tempFoodO.addAllergens(milk, soy, treeOrPNut, gluten,
+                                    fishShell, egg, wheat);
+                            foodItems.add(tempFoodO);
                             break;
-
                         }
 
                         if (isProtein) {
@@ -212,7 +275,10 @@ public class Tracker extends ListActivity implements View.OnClickListener {
                             isProtein = false;
                             obtainedProtein = true;
 
-                        } if (isFat) {
+                        }
+
+                        if (isFat) {
+
                             String temp = cur.split("g")[0];
 
                             currentFat = Float.valueOf(temp);
@@ -220,7 +286,9 @@ public class Tracker extends ListActivity implements View.OnClickListener {
                             isFat = false;
                             obtainedFat = true;
 
-                        }  if (isCarbs) {
+                        }
+
+                        if (isCarbs) {
                             String temp = cur.split("g")[0];
 
                             currentCarb = Float.valueOf(temp);
@@ -228,16 +296,14 @@ public class Tracker extends ListActivity implements View.OnClickListener {
                             isCarbs = false;
                             obtainedCarbs = true;
 
-
                         }
-
 
                         if (cur.contains("Calories" + "\u00a0")) {
 
                             currentCal = Integer.parseInt(cur.split("\u00a0")[1]);
                             obtainedCalories = true;
 
-                        } else if (cur.contains("Protein"+ "\u00a0")) {
+                        } else if (cur.contains("Protein" + "\u00a0")) {
 
                             isProtein = true;
 
@@ -250,11 +316,8 @@ public class Tracker extends ListActivity implements View.OnClickListener {
                             isCarbs = true;
 
                         }
-
-
-
-                    }
-                }
+                    } // End of for loop with "nutrition" tag | Loop that scrapes nutritional info
+                } // End of for loop with "foods" tag | Loop that grabs each food items name
 
                 Looper.myLooper().quit();
 
@@ -263,10 +326,11 @@ public class Tracker extends ListActivity implements View.OnClickListener {
                     public void run() {
                         List<String> thelist = new ArrayList<>();
 
-                        for (FoodObject e: foodItems)
+                        for (FoodObject e : foodItems)
                             thelist.add(e.getTag());
 
-                        arrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item, R.id.tvName,thelist);
+                        arrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item,
+                                R.id.tvName, thelist);
 
                         getListView().setAdapter(arrayAdapter);
 
@@ -275,34 +339,14 @@ public class Tracker extends ListActivity implements View.OnClickListener {
                 });
 
 
-
             } // end run
         }; // end runnable
         Thread thread = new Thread(runnable);
         thread.start();
-        System.out.println("end");
 
-        ListView lv = getListView();
-        lv.setAdapter(arrayAdapter);
 
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                calCount += foodItems.get(position).getCal();
-                push(foodItems.get(position));
-                updateCount();
-            }
-        });
-    }
+    } // end updateList
 
-    // When button clicked add calories.
-    public void onClick(View v) {
-        switch (v.getId()) {
-
-        }
-
-        updateCount();
-    }
 
     private void updateCount() {
         count.setText(String.valueOf(calCount));
@@ -327,7 +371,7 @@ public class Tracker extends ListActivity implements View.OnClickListener {
     }
 
     // Get time of day and returns String for Breakfast, Lunch, Dinner, or Closed.
-    public String mealTime(){
+    public String getMealTime(){
         Calendar c = Calendar.getInstance();
         int hourOfDay = c.get(Calendar.HOUR_OF_DAY);
         String mealHour = "Closed";
@@ -341,11 +385,32 @@ public class Tracker extends ListActivity implements View.OnClickListener {
 
         return mealHour;
     }
-}
-/*
-    <tbody>
-        <tr>...<tr> Contains all of the information for each food item
 
-    It looks like "labelrecipe" is the tag i'll be using to find to scrape the website
-    <div class="labelrecipe">Texas Beef Chili</div>
- */
+    // Takes in dinning hall name(ex. Cowell/Stevenson), and returns it's web number.
+    public int webLocation(String location){
+        int locationNumber = 0;
+        if(location == "Cowell/Stevenson"){
+            locationNumber = 5;
+        }else if(location == "Crown/Merrill"){
+            locationNumber = 20;
+        }else if(location == "Porter/Kresge") {
+            locationNumber = 25;
+        }else if(location == "Eight/Oakes"){
+            locationNumber = 30;
+        }else if(location == "Nine/Ten"){
+            locationNumber = 40;
+        }
+        return locationNumber;
+    }
+
+    private String getDateString() {
+
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        System.out.println(date);
+        String currentMonth = date.substring(5, 7);          // From current date
+        String currentDay = date.substring(8);           // From current date
+        String currentYear = date.substring(0, 4);        // From current date
+
+        return currentMonth + "%2F" + currentDay + "%2F" + currentYear;
+    }
+}
